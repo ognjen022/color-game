57d55a8da5f220a70151eb9aaba67a50805f4f9b
ognjen022 <ognjen022@gmail.com>
2020-06-18 13:18:23 +0200

    Fix server

--- server/server.js
+++ server/server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(publicPath));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(publicPath);
   console.log('Server listening for requests.');
 });
