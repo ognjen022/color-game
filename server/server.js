4d6a73f2997d7a64332af035082baf6e7d36fc82
ognjen022 <ognjen022@gmail.com>
2020-06-18 13:15:55 +0200

    Fix server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function (req, res) {
+
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
