d756faac6463395bbe9293242b60724d6db124f7
ognjen022 <ognjen022@gmail.com>
2020-06-18 13:13:01 +0200

    Change server

--- server/server.js
+++ server/server.js
@@ -4,10 +4,9 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 5000;
 
-app.use(express.static(publicPath));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
